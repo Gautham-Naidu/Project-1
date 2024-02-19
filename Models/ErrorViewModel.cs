02e475eae4da343409afcd807aa5b7fc9dc76b0d
Abdul Wasay <khiddu.m@gmail.com>
2024-02-19 10:25:07 +0530

    created new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Practiceproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+     public class ErrorViewModel111
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
